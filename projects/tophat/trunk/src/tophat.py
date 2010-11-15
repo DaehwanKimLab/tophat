b866757331438da62f489aeca77db3bd3d6352e9
cole <cole@e53bd72b-32bf-4dec-aa00-2f942a4fcc91>
2010-11-15 00:51:24 +0000

    version bump to 1.1.3

--- projects/tophat/trunk/src/tophat.py
+++ projects/tophat/trunk/src/tophat.py
@@ -2078,7 +2078,7 @@ def prog_path(program):
 
 # FIXME: this should get set during the make dist autotools phase of the build
 def get_version():
-   return "1.1.0"
+   return "1.1.3"
 
 def main(argv=None):
     warnings.filterwarnings("ignore", "tmpnam is a potential security risk")
