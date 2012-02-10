2d0d25ee6985fc5de8c2c91b79474729bbd6daa4
infphilo <infphilo@e53bd72b-32bf-4dec-aa00-2f942a4fcc91>
2012-02-10 20:17:00 +0000

    map2gtf now handles bam records directly, which is way faster - later, we can merge map2gtf into fix_map_ordering sometime soon for increased speed

--- src/map2gtf.cpp
+++ src/map2gtf.cpp
@@ -12,194 +12,217 @@ void m2g_print_usage()
             << " alignments.bwtout out_file.sam" << std::endl;
 }
 
-Map2GTF::Map2GTF(std::string gtf_fname, std::string reads_fname) :
-    gtf_fname_(gtf_fname), reads_fname_(reads_fname), refSeqTable_(true)
+Map2GTF::Map2GTF(const std::string& gtf_fname, const std::string& in_fname) :
+    gtf_fname_(gtf_fname), in_fname_(in_fname), refSeqTable_(true)
 {
-    gtf_fhandle_ = fopen(gtf_fname_.c_str(), "r");
-    if (gtf_fhandle_ == NULL)
+  gtf_fhandle_ = fopen(gtf_fname_.c_str(), "r");
+  if (gtf_fhandle_ == NULL)
     {
-        std::cerr << "FATAL: Couldn't open annotation: " << gtf_fname_
+      std::cerr << "FATAL: Couldn't open annotation: " << gtf_fname_
                 << std::endl;
-        exit(1);
+      exit(1);
     }
-    std::cout << "Reading the annotation file: " << gtf_fname_ << std::endl;
-    gtfReader_.init(gtf_fhandle_, true); //only recognizable transcripts will be loaded
-    gtfReader_.readAll();
-
-    std::cout << "Initializing the BAMHitFactory." << std::endl;
-    hitFactory_ = new BAMHitFactory(readTable_, refSeqTable_);
-    std::cout << "Done with init samfactory" << std::endl;
-
-    // read bam file
-    std::cout << "Initializing the HitStream." << std::endl;
-    hitStream_ = new HitStream(reads_fname, hitFactory_, false, true,
-			       true, true, true, true);
-    
-    std::cout << "Done with initializtion. " << std::endl;
+  std::cout << "Reading the annotation file: " << gtf_fname_ << std::endl;
+  gtfReader_.init(gtf_fhandle_, true); //only recognizable transcripts will be loaded
+  gtfReader_.readAll();
+  
+  in_fhandle_=samopen(in_fname_.c_str(), "rb", 0);
+  if (in_fhandle_ == NULL)
+    {
+      std::cerr << "FATAL: Couldn't open input bam file: " << in_fname_
+		<< std::endl;
+      exit(1);
+    }
+  in_sam_header_ = in_fhandle_->header;
+  
+  std::cout << "Done with initializtion. " << std::endl;
 }
 
 Map2GTF::~Map2GTF()
 {
-    std::cout << "map2gtf has completed. Cleaning up." << std::endl;
-    if (gtf_fhandle_ != NULL && fclose(gtf_fhandle_))
+  std::cout << "map2gtf has completed. Cleaning up." << std::endl;
+  if (gtf_fhandle_ != NULL && fclose(gtf_fhandle_))
     {
-        std::cerr << "Warning: Error closing annotation: " << gtf_fname_
+      std::cerr << "Warning: Error closing annotation: " << gtf_fname_
                 << std::endl;
     }
-
-    if (zpacker != "")
+  
+  if (in_fhandle_ != NULL)
     {
-        std::cout << "Closing the zpacker. " << std::endl;
-        reads_pipe_.close();
+      samclose(in_fhandle_);
     }
-    else
-    {
-        if (reads_fhandle_ != NULL && fclose(reads_fhandle_))
-        {
-            std::cerr << "Warning: Error closing reads: " << reads_fname_
-            << std::endl;
-        }
-    }
-
-    delete hitFactory_;
-    delete hitStream_;
-    std::cout << "Done. Thanks!" << std::endl;
+  
+  std::cout << "Done. Thanks!" << std::endl;
 }
 
-void Map2GTF::convert_coords(std::string out_fname, std::string sam_header)
+//
+bool Map2GTF::next_read_hits(vector<bam1_t*>& hits, size_t& num_hits)
 {
-  // FIXME: come up with a more efficient layout for doing this
-  GBamWriter bam_writer(out_fname.c_str(), sam_header.c_str());
+  long read_id = 0;
+  if (hits.size() > num_hits)
+    {
+      bam1_t* temp = hits[num_hits];
+      hits[num_hits] = hits.front();
+      hits.front() = temp;
+
+      num_hits = 1;
+      char* name = bam1_qname(hits.front());
+      read_id = atol(name);
+    }
+  else
+    num_hits = 0;
+
+  while (true)
+    {
+      bam1_t* hit = NULL;
+      if (num_hits >= hits.size())
+	hits.push_back(bam_init1());
+
+      hit = hits[num_hits];
+
+      if (samread(in_fhandle_, hit) <= 0)
+	{
+	  for (size_t i = num_hits; i < hits.size(); ++i)
+	      bam_destroy1(hits[i]);
+
+	  hits.erase(hits.begin() + num_hits, hits.end());
+	  break;
+	}
+
+      char* name = bam1_qname(hit);
+      long temp_read_id = atol(name);
+      if (num_hits == 0)
+	{
+	  read_id = temp_read_id;
+	}
+      else if (read_id != temp_read_id)
+	{
+	  break;
+	}
+	
+      ++num_hits;
+    }
+  
+  return num_hits > 0;
+}
+
+void Map2GTF::convert_coords(const std::string& out_fname, const std::string& sam_header)
+{
+  samfile_t* out_sam_header_file = samopen(sam_header.c_str(), "r", 0);
+  if (out_sam_header_file == NULL)
+    std::cerr << "Error opening sam header: " << sam_header << std::endl;
+
+  out_sam_header_ = out_sam_header_file->header;
+  
+  samfile_t* bam_writer = samopen(out_fname.c_str(), "wb", out_sam_header_);
+  if (bam_writer == NULL)
+    std::cerr << "Error opening bam file for writing: " << out_fname << std::endl;
+
+  ref_to_id_.clear();
+  for (int i = 0; i < out_sam_header_->n_targets; ++i)
+    {
+      ref_to_id_[out_sam_header_->target_name[i]] = i;
+    }
   
   std::vector<TranscriptomeHit> read_list;
-  
   GffObj* p_trans = NULL;
   
   HitsForRead hit_group;
-  char read_name[MAX_READ_NAME_LEN]; // FIXME: use the macro size
-  
   std::vector<TranscriptomeHit>::iterator bh_it;
   std::vector<TranscriptomeHit>::iterator bh_unique_it;
   BowtieHit bwt_hit;
+
+  vector<bam1_t*> hits;
+  size_t num_hits = 0;
   // a hit group is a set of reads with the same name
-  while (hitStream_->next_read_hits(hit_group))
+  while (next_read_hits(hits, num_hits))
     {
-      TranscriptomeHit primary_hit;
-      for (size_t i = 0; i < hit_group.hits.size(); ++i)
+      for (size_t i = 0; i < num_hits; ++i)
         {
-	  // TODO: check if read is unmapped.
-	  // if so, figure out what to do with it
-	  bwt_hit = hit_group.hits[i];
-	  sprintf(read_name, "%u", bwt_hit.insert_id());
-	  
-	  size_t trans_idx = atoi(refSeqTable_.get_name(bwt_hit.ref_id()));
+	  bam1_t* hit = hits[i];
+	  const char* target_name = in_sam_header_->target_name[hit->core.tid];
+	  size_t trans_idx = atoi(target_name);
+	    
 	  p_trans = gtfReader_.gflst.Get(trans_idx);
-	  TranscriptomeHit converted_out(p_trans);
-	  trans_to_genomic_coords(read_name, hitFactory_, bwt_hit,
-				  converted_out);
-
-	  vector<string> aux_fields;
-	  tokenize(bwt_hit.hitfile_rec().c_str(), "\t", aux_fields);
-	  for (size_t j = 11; j < aux_fields.size(); ++j)
-	    {
-	      const string& aux = aux_fields[j];
-	      if (aux.substr(0, 2) == "XS" || aux.substr(0,2) == "NM")
-		continue;
-
-	      converted_out.aux_fields.push_back(aux);
-	    }
-
-	  string strand = "XS:A:";
-	  strand.push_back(converted_out.trans->strand);
-	  converted_out.aux_fields.push_back(strand);
-
-	  // since Bowtie2 outputs quality values only for the primary hit,
-	  // we need to make sure that the primary hit is output.
-	  if (bowtie2 && bwt_hit.qual().length() > 0 && bwt_hit.qual()[0] != ' ')
-	    primary_hit = converted_out;
-	  else
-	    read_list.push_back(converted_out);
+	  TranscriptomeHit converted_out(hit, p_trans);
+	  trans_to_genomic_coords(converted_out);
+	  
+	  read_list.push_back(converted_out);
         }
       
       // XXX: Fine for now... should come up with a more efficient way though
       // FIXME: Take frag length into consideration when filtering
       std::sort(read_list.begin(), read_list.end());
       bh_unique_it = std::unique(read_list.begin(), read_list.end());
-
-      if (bowtie2)
-	print_bamhit(bam_writer, read_name, primary_hit.hit, primary_hit.trans->getRefName(), primary_hit.trans->getRefName(),
-		     primary_hit.hit.seq().c_str(), primary_hit.hit.qual().c_str(), true, &(primary_hit.aux_fields));
-      
-      
       for (bh_it = read_list.begin(); bh_it != bh_unique_it; ++bh_it)
         {
-	  if (bowtie2 && primary_hit.hit == bh_it->hit)
-	    continue;
-	  
-	  print_bamhit(bam_writer, read_name, bh_it->hit, bh_it->trans->getRefName(), bh_it->trans->getRefName(),
-		       bh_it->hit.seq().c_str(), bh_it->hit.qual().c_str(), true, &(bh_it->aux_fields));
+	  samwrite(bam_writer, bh_it->hit);
         }
       read_list.clear();
     }
-  
-  //    fclose(out_sam);
+
+  for (size_t i = 0; i < hits.size(); ++i)
+    {
+      bam_destroy1(hits[i]);
+    }
+  hits.clear();
+
+  samclose(bam_writer);
 }
 
-void trans_to_genomic_coords(const char* read_name, HitFactory* hitFactory,
-			     const BowtieHit& in, TranscriptomeHit& out)
+void Map2GTF::trans_to_genomic_coords(TranscriptomeHit& hit)
 //out.trans must already have the corresponding GffObj*
 {
-    std::string read_id(read_name);
-    std::string ref_name(out.trans->getRefName());
-    bool spliced = false;
-    std::vector<CigarOp> cig_list;
-
     // read start in genomic coords
     size_t read_start = 0;
 
-    GList<GffExon>& exon_list = out.trans->exons;
+    GList<GffExon>& exon_list = hit.trans->exons;
     GffExon* cur_exon;
     GffExon* next_exon;
-    size_t cur_pos;
-    size_t match_length;
-    size_t miss_length;
-    size_t cur_intron_len = 0;
+    int cur_pos;
+    int match_length;
+    int miss_length;
+    int cur_intron_len = 0;
     int i = 0;
 
+    static const int MAX_CIGARS = 256;
+    int cigars[MAX_CIGARS];
+    int num_cigars = 0;
+
     // TODO: Check this return value
-    bool ret_val = get_read_start(&exon_list, in.left(), read_start, i);
+    bool ret_val = get_read_start(&exon_list, hit.hit->core.pos, read_start, i);
     cur_pos = read_start;
-
-    const std::vector<CigarOp>& cigars = in.cigar();
-    for (size_t c = 0; c < cigars.size(); ++c)
+    for (int c = 0; c < hit.hit->core.n_cigar; ++c)
       {
-	const CigarOp& cigar = cigars[c];
+	int opcode = bam1_cigar(hit.hit)[c] & BAM_CIGAR_MASK;
+	int length = bam1_cigar(hit.hit)[c] >> BAM_CIGAR_SHIFT;
 
-	if (cigar.opcode == INS)
-	  cig_list.push_back(cigar);
+	if (opcode == BAM_CINS)
+	  {
+	    cigars[num_cigars] = opcode | (length << BAM_CIGAR_SHIFT);
+	    ++num_cigars;
+	  }
 	
-	if (cigar.opcode != MATCH && cigar.opcode != DEL)
+	if (opcode != BAM_CMATCH && opcode != BAM_CDEL)
 	  continue;
 	
-	size_t remaining_length = cigar.length;	
+	int remaining_length = length;
 	for (; i < exon_list.Count(); ++i)
 	  {
 	    cur_exon = exon_list.Get(i);
-	    if (cur_pos >= cur_exon->start &&
-		cur_pos + remaining_length - 1 <= cur_exon->end) // read ends in this exon
+	    if (cur_pos >= (int)cur_exon->start &&
+		cur_pos + remaining_length - 1 <= (int)cur_exon->end) // read ends in this exon
 	      {
-		CigarOp cig(cigar.opcode, remaining_length);
-		cig_list.push_back(cig);
-
+		cigars[num_cigars] = opcode | (remaining_length << BAM_CIGAR_SHIFT);
+		++num_cigars;
 		cur_pos += remaining_length;
 		break;
 	      }
 	    
 	    // shouldn't need the check... can switch to a regular "else"
-	    else if (cur_pos >= cur_exon->start &&
-		     cur_pos + remaining_length - 1 > cur_exon->end)// read is spliced and overlaps this exon
+	    else if (cur_pos >= (int)cur_exon->start &&
+		     cur_pos + remaining_length - 1 > (int)cur_exon->end)// read is spliced and overlaps this exon
 	      {
-		spliced = true;
 		// XXX: This should _never_ go out of range.
 		// get the max length that fits in this exon, go to next exon
 		// cur_pos should be the next exon start
@@ -209,8 +232,8 @@ void trans_to_genomic_coords(const char* read_name, HitFactory* hitFactory,
 		match_length = cur_exon->end - cur_pos + 1;
 		if (match_length > 0)
 		  {
-		    CigarOp cig(cigar.opcode, match_length);
-		    cig_list.push_back(cig);
+		    cigars[num_cigars] = opcode | (match_length << BAM_CIGAR_SHIFT);
+		    ++num_cigars;
 		  }
 		
 		// XXX: DEBUG
@@ -218,7 +241,7 @@ void trans_to_genomic_coords(const char* read_name, HitFactory* hitFactory,
 		  {
 		    std::cerr << "trying to access: " << i + 1 << " when size is: "
 			      << exon_list.Count() << std::endl;
-		    print_trans(out.trans, in, remaining_length, match_length, cur_pos,
+		    print_trans(hit.trans, hit.hit, remaining_length, match_length, cur_pos,
 				read_start);
 		    exit(1);
 		  }
@@ -229,8 +252,9 @@ void trans_to_genomic_coords(const char* read_name, HitFactory* hitFactory,
 		// and this
 		miss_length = next_exon->start - cur_exon->end - 1;
 		cur_intron_len += miss_length;
-		CigarOp miss_cig(REF_SKIP, miss_length);
-		cig_list.push_back(miss_cig);
+
+		cigars[num_cigars] = BAM_CREF_SKIP | (miss_length << BAM_CIGAR_SHIFT);
+		++num_cigars;
 		
 		cur_pos += match_length + miss_length;
 		remaining_length -= match_length;
@@ -239,17 +263,47 @@ void trans_to_genomic_coords(const char* read_name, HitFactory* hitFactory,
 	  }
       }
 
-    bool antisense_splice = (spliced && out.trans->strand=='-'); //transcript strand <=> splice strand (if spliced)
-    read_start -= 1; // handle the off-by-one problem
-    out.hit = hitFactory->create_hit(read_id, ref_name, "",
-				     static_cast<int> (read_start), cig_list, in.antisense_align(),
-				     antisense_splice, in.edit_dist(), in.splice_mms(), in.end());
-    out.hit.seq(in.seq());
-    out.hit.qual(in.qual());
+    hit.hit->core.tid = ref_to_id_[hit.trans->getRefName()];
+    hit.hit->core.pos = read_start - 1;
+    hit.hit->core.flag &= ~BAM_FSECONDARY;
+    
+    int old_n_cigar = hit.hit->core.n_cigar;
+    if (num_cigars != old_n_cigar)
+      {
+	int data_len = hit.hit->data_len + 4 * (num_cigars - old_n_cigar);
+	int m_data = max(data_len, hit.hit->m_data);
+	kroundup32(m_data);
+	
+	uint8_t* data = (uint8_t*)calloc(m_data, 1);
+
+	int copy1_len = (uint8_t*)bam1_cigar(hit.hit) - hit.hit->data;
+	memcpy(data, hit.hit->data, copy1_len);
+
+	int copy2_len = num_cigars * 4;
+	memcpy(data + copy1_len, cigars, copy2_len);
+
+	int copy3_len = hit.hit->data_len - copy1_len - (old_n_cigar * 4);
+	memcpy(data + copy1_len + copy2_len, bam1_seq(hit.hit), copy3_len);
+
+	hit.hit->core.n_cigar = num_cigars;
+	
+	free(hit.hit->data);
+	hit.hit->data = data;
+	hit.hit->data_len = data_len;
+	hit.hit->m_data = m_data;
+      }
+
+    char strand = hit.trans->strand;
+    uint8_t* ptr = bam_aux_get(hit.hit, "XS");
+    if (ptr)
+      bam_aux_del(hit.hit, ptr);
+
+    if (strand == '+' || strand == '-')
+      bam_aux_append(hit.hit, "XS", 'A', 1, (uint8_t*)&strand);
 }
 
-void print_trans(GffObj* trans, const BowtieHit& in, size_t rem_len,
-        size_t match_len, size_t cur_pos, size_t start_pos)
+void print_trans(GffObj* trans, const bam1_t* in, size_t rem_len,
+		 size_t match_len, size_t cur_pos, size_t start_pos)
 {
     GffExon* p_exon;
     std::cerr << "\tCur_pos: " << cur_pos << " remaining: " << rem_len
@@ -264,8 +318,8 @@ void print_trans(GffObj* trans, const BowtieHit& in, size_t rem_len,
     }
     std::cerr << std::endl;
 
-    std::cerr << "Read_id: " << in.insert_id() << std::endl;
-    std::cerr << "\tgff_start: " << in.left() << "\tgenome_start: "
+    std::cerr << "Read_id: " << bam1_qname(in) << std::endl;
+    std::cerr << "\tgff_start: " << in->core.pos << "\tgenome_start: "
             << start_pos << std::endl;
 }
 
@@ -315,17 +369,16 @@ int main(int argc, char *argv[])
     }
   
   std::string gtf_file(argv[optind++]);
-  std::string reads_file(argv[optind++]);
+  std::string in_fname(argv[optind++]);
   std::string out_fname(argv[optind++]);
   
-  if (gtf_file == "" || reads_file == "" || out_fname == "")
+  if (gtf_file == "" || in_fname == "" || out_fname == "")
     {
       m2g_print_usage();
       exit(1);
     }
   
-  Map2GTF gtfMapper(gtf_file, reads_file);
-  //    gtfMapper.convert_coords(out_fname);
+  Map2GTF gtfMapper(gtf_file, in_fname);
   gtfMapper.convert_coords(out_fname, sam_header);
   
   return 0;
--- src/map2gtf.h
+++ src/map2gtf.h
@@ -38,58 +38,90 @@
  * XXX: This class currently assumes someone used the script in TopHat to map
  *      the reads already. It also depends on that same format.
  */
+class TranscriptomeHit;
 class Map2GTF
 {
 public:
-    Map2GTF(std::string gtf_fname, std::string sam_fname);
+    Map2GTF(const std::string& gtf_fname, const std::string& sam_fname);
     ~Map2GTF();
     // Write out to a BAM file
-    void convert_coords(std::string out_fname, std::string sam_header);
+    bool next_read_hits(std::vector<bam1_t*>& hits, size_t& num_hits);
+    void convert_coords(const std::string& out_fname, const std::string& sam_header);
+    void trans_to_genomic_coords(TranscriptomeHit& hit);
 
 private:
     GffReader gtfReader_;
 
     std::string gtf_fname_;
-    std::string reads_fname_;
+    std::string in_fname_;
 
     FILE* gtf_fhandle_;
-    FILE* reads_fhandle_;
-    FZPipe reads_pipe_;
+    samfile_t* in_fhandle_;
+    bam_header_t* in_sam_header_;
+
+    map<string, int> ref_to_id_;
+    bam_header_t* out_sam_header_;
 
     ReadTable readTable_;
     RefSequenceTable refSeqTable_;
 
-    HitFactory* hitFactory_;
-    HitStream* hitStream_;
-
     Map2GTF(); // Don't want anyone calling the constructor w/o options
 };
 
-class TranscriptomeHit {
+class TranscriptomeHit
+{
  public:
-  BowtieHit hit;
+  bam1_t* hit;
   GffObj* trans;
-  vector<string> aux_fields;
- TranscriptomeHit(GffObj* t=NULL):hit() {
-    trans=t;
+  TranscriptomeHit(bam1_t* h = NULL, GffObj* t = NULL)
+    {
+      hit = h;
+      trans = t;
+    }
+  bool operator==(const TranscriptomeHit& th) const
+  {
+    if (hit->core.tid != th.hit->core.tid)
+      return false;
+    
+    if (hit->core.pos != th.hit->core.pos)
+      return false;
+    
+    if (hit->core.n_cigar != th.hit->core.n_cigar)
+      return false;
+    
+    for (int i = 0; i < hit->core.n_cigar; ++i)
+      {
+	if (bam1_cigar(hit)[i] != bam1_cigar(th.hit)[i])
+	  return false;
+      }
+    
+    return true;
   }
-  bool operator==(const TranscriptomeHit& th) const {
-    return (th.hit == hit);
-  }
-  bool operator<(const TranscriptomeHit& th) const {
-    return (th.hit < hit);
-  }
-  
+  bool operator<(const TranscriptomeHit& th) const
+  {
+    if (hit->core.tid != th.hit->core.tid)
+      return hit->core.tid < th.hit->core.tid;
+    
+    if (hit->core.pos != th.hit->core.pos)
+      return hit->core.pos < th.hit->core.pos;
+    
+    if (hit->core.n_cigar != th.hit->core.n_cigar)
+      return hit->core.n_cigar < th.hit->core.n_cigar;
+    
+    for (int i = 0; i < hit->core.n_cigar; ++i)
+      {
+	if (bam1_cigar(hit)[i] != bam1_cigar(th.hit)[i])
+	  return bam1_cigar(hit)[i] < bam1_cigar(th.hit)[i];
+      }
+    
+    return false;
+  }  
 };
-
-
-void trans_to_genomic_coords(const char* read_name, HitFactory* hitFactory,
-			     const BowtieHit& in, TranscriptomeHit& out);
 
 bool get_read_start(GList<GffExon>* exon_list, size_t gtf_start,
 		    size_t& genome_start, int& exon_idx);
 
-void print_trans(GffObj* trans, const BowtieHit& in, size_t rem_len,
+void print_trans(GffObj* trans, const bam1_t* in, size_t rem_len,
 		 size_t match_len, size_t cur_pos, size_t start_pos);
 
 #endif /* _MAP2GTF_H_ */
