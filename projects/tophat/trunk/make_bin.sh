05bdc13e2281a799abcd4e1794b15330116657ff
gpertea <gpertea@e53bd72b-32bf-4dec-aa00-2f942a4fcc91>
2014-09-25 22:37:29 +0000

    Updated documentation and other files for 2.0.13

--- projects/tophat/trunk/make_bin.sh
+++ projects/tophat/trunk/make_bin.sh
@@ -2,14 +2,25 @@
 
 #simple script to pack up a precompiled binary package
 if [[ -z "$1" ]]; then
- echo -e "Usage:\n./make_bin.sh <binary_package_name> [<bam_prefix> <boost_prefix>]"
+ echo -e "Usage:\n./make_bin.sh <binary_package_name> [<boost_prefix>]"
+ echo "  <binary_package_name> cannot be a full path, but only a file name"
+ echo "  e.g.: tophat-2.0.13.Linux_x86_64"
  exit 1
 fi
-echo "packing up $1.tar.gz, using BAM installation in $2, BOOST installation in $3"
-/bin/rm -rf $1 $1.tar.gz
-mkdir $1
-make clean
-make distclean
+#echo "packing up $1.tar.gz, using BOOST installation in $2"
+build="$1"
+boostpre="$2"
+if [[ -z "$boostpre" ]]; then
+  echo "Preparing binary package $build.tar.gz using the system BOOST installation"
+else
+  echo "Preparing binary package $build.tar.gz using BOOST from $boostpre"
+fi
+/bin/rm -rf $build $build.tar.gz
+mkdir $build
+if [[ -f Makefile ]]; then
+  make clean
+  make distclean
+fi
 if [[ $(uname -m) = "x86_64" ]]; then
  echo "Linking statically on x86_64.."
  export LDFLAGS="-static-libgcc -static-libstdc++"
@@ -18,23 +29,16 @@ if [[ $(uname) = "Darwin" ]]; then
  export CFLAGS="-mmacosx-version-min=10.6"
 fi
 
-
-l2="$2"
-l3="$3"
-if [[ -z "$l3" ]]; then
-  l3="$l2"
-fi
-
-./configure --prefix=`pwd`/$1 --with-bam=$l2 --with-boost=$l3
+./configure --prefix=`pwd`/$build --with-boost=$boostpre
 sed -e 's|__PREFIX__||' src/tophat2.in > src/tophat2
 make
 make install
-cp $1/bin/* $1
+cp $build/bin/* $build
 
-cp README $1
-cp COPYING $1
-cp AUTHORS $1
+cp README $build
+cp COPYING $build
+cp AUTHORS $build
 
-/bin/rm -rf $1/bin
+/bin/rm -rf $build/bin
 
-tar cvfz $1.tar.gz $1
+tar cvfz $build.tar.gz $build
