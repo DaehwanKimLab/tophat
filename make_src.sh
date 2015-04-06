516d51c5da985324e41e52409060321bb2713b90
infphilo <infphilo@gmail.com>
2015-04-06 10:46:11 -0400

    fixed make_src.sh

--- make_src.sh
+++ make_src.sh
@@ -5,7 +5,8 @@
 echo "packing up $1.tar.gz"
 mkdir $1
 
-cp -r --preserve=all *.m4  build-aux src config.h.in configure.ac Makefile.am Makefile.in configure ChangeLog NEWS README COPYING AUTHORS INSTALL THANKS annotation $1
+make clean
+cp -r --preserve=all *.m4 src config.h.in configure.ac Makefile.am Makefile.in configure install-sh config.sub config.guess depcomp ChangeLog NEWS README COPYING AUTHORS INSTALL THANKS $1
 (cd $1; find -name .svn -exec rm -rf {} \;)
 tar cvzf $1.tar.gz $1
 rm -rf $1
